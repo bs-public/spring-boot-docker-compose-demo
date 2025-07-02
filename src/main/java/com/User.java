236614d1cce7d3a11ed15035ffe82a14191d4f55
bs <26665808+bs-public@users.noreply.github.com>
2025-07-02 18:07:58 +1000

    Add User entity

---
+++ src/main/java/com/User.java
@@ -0,0 +1,35 @@
+package com;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class User {
+
+  @Id
+  @GeneratedValue
+  private Integer id;
+
+  @Column
+  private String name;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
